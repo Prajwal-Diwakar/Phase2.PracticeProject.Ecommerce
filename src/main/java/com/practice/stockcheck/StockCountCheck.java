1bf5bab586181b81f70baff8fba48a915ac3a2a9
Prajwal Diwakar <prajwalkinnigoli59@gmail.com>
2023-12-21 22:14:43 +0530

    committed

--- src/main/java/com/practice/stockcheck/StockCountCheck.java
+++ src/main/java/com/practice/stockcheck/StockCountCheck.java
@@ -1,25 +1,35 @@
 package com.practice.stockcheck;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import org.testng.annotations.Test;
 
 import com.practiceProject.ecommerce.ProductStockCount;
 
 public class StockCountCheck {
 	
-	@Test
-	public void testCheckStockAvailability() {
-	String productName = "Mobile";
-	ProductStockCount productDAO = new ProductStockCount();
-	int stockQuantity = productDAO.getStockQuantity(productName);
-	// Display stock information
-	
-	System.out.println("| Stock Availability of the selected product|");
-	
-	System.out.println("| Product Name | Stock Quantity |");
-	
-	System.out.printf("| %-13s | %-15d |%n", productName, stockQuantity);
-	
+public static void main (String[] args) throws Exception {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce","root", "1234");
+		
+		Statement st= con.createStatement();
+		ResultSet rs=st.executeQuery("select * from eproduct");
+		
+		while (rs.next()) 
+		{
+			int ID= rs.getInt("ID");
+			String name =rs.getString("Name");
+			int price=rs.getInt("Price");
+			Date date_added=rs.getDate("Date_added");
+			System.out.println(ID+"  "+name+"   "+price+"    "+ date_added);
+		}
+		st.close();
+		con.close();
 	}
-	}
-
+}
 
